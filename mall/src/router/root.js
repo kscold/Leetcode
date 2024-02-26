3fc10521ac3b76825c862aef2b3cef6f44a64356
kscold <201921339@sangmyung.kr>
2024-02-27 03:51:12 +0900

    #2 - React Router의 <Outlet> 기능 설정
    React Router 최신버전 라이브러리의 기능으로 root.js에서 패스 설정함에 따라 따로 children 컴포넌트로 설정하지 않아도 <Outlet> 설정으로 레이아웃을 설정 가능

--- mall/src/router/root.js
+++ mall/src/router/root.js
@@ -5,6 +5,9 @@ const Loading = <div className="bg-red-700">Loading...</div>;
 const Main = lazy(() => import('../pages/MainPage'));
 const About = lazy(() => import('../pages/AboutPage'));
 
+const TodoIndex = lazy(() => import('../pages/todo/IndexPage'));
+const TodoList = lazy(() => import('../pages/todo/ListPage'));
+
 const root = createBrowserRouter([
   // ReactRouter설정으로 객체형식으로 패스를 설정
   {
@@ -18,10 +21,29 @@ const root = createBrowserRouter([
   {
     path: 'about',
     element: (
-      <Suspense fallback={About}>
+      <Suspense fallback={Loading}>
         <About />
       </Suspense>
     ),
+  },
+  {
+    path: 'todo',
+    element: (
+      <Suspense fallback={Loading}>
+        <TodoIndex />
+      </Suspense>
+    ),
+    children: [
+      {
+        // 하위 라우팅을 분기
+        path: 'list',
+        element: (
+          <Suspense fallback={Loading}>
+            <TodoList />
+          </Suspense>
+        ),
+      },
+    ],
   },
 ]);
 
