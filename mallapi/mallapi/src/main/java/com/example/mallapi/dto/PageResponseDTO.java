ac84d8077a9e7d637edda610151f63c4df9ea145
kscold <201921339@sangmyung.kr>
2024-03-04 18:05:40 +0900

    #7 - 백엔드 페이징 정보가 정확히 올라가지 않아서 수정
    백엔드 페이징 세부 정보들이 정확히 호출되지 않아서 수정

--- mallapi/mallapi/src/main/java/com/example/mallapi/dto/PageResponseDTO.java
+++ mallapi/mallapi/src/main/java/com/example/mallapi/dto/PageResponseDTO.java
@@ -23,9 +23,11 @@ public class PageResponseDTO<E> {
     @Builder(builderMethodName = "withAll") // 빌더 패턴을 사용하는데 빌터 패턴에 메서드 이름을 명시
     public PageResponseDTO(List<E> dtoList, PageRequestDTO pageRequestDTO, long total) { // 받는 생성자를 엔티티와 DTO 총 페이지 수를 받도록 설정
 
-        this.dtoList = dtoList;
+        /*this.dtoList = dtoList;
         this.pageRequestDTO = pageRequestDTO;
         this.totalCount = (int) total;
+
+        this.current = pageRequestDTO.getPage(); // 현재 페이징 위치
 
         // 끝페이지 end 값 구하기
         int end = (int) (Math.ceil(pageRequestDTO.getPage() / 10.0)) * 10;
@@ -42,12 +44,36 @@ public class PageResponseDTO<E> {
         // 다음으로 가는 링크 계산하기 위한 변수 선언
         this.next = totalCount > end * pageRequestDTO.getSize();
 
-        this.pageNumList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList()); // int[] -> List<Integer> 로 변환
+        this.pageNumList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList()); // int[] -> List<Integer> 로 변환*/
+
+        this.dtoList = dtoList;
+        this.pageRequestDTO = pageRequestDTO;
+        this.totalCount = (int) total;
+
+        this.current = pageRequestDTO.getPage(); // 현재 페이징 위치
+
+        int end = (int) (Math.ceil(pageRequestDTO.getPage() / 10.0)) * 10;
+
+        int start = end - 9;
+
+        int last = (int) (Math.ceil((totalCount / (double) pageRequestDTO.getSize())));
+
+        end = end > last ? last : end;
+
+        this.prev = start > 1;
+
+
+        this.next = totalCount > end * pageRequestDTO.getSize();
+
+        this.pageNumList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+
 
         this.prevPage = prev ? start - 1 : 0; // 2 페이징 이상이면, 하나 전 페이징으로 감소 이하면 0 페이지으로 설정
+        this.nextPage = next ? 0 : end + 1; // 2 페이징 이상히면, 앞의 페이징으로 증가 이하면 0 페이지로 설정
+      /*  this.prevPage = current - 1; // 이전 페이징 번호 설정
+        this.nextPage = current + 1;// 다음 페이징 번호 설정*/
 
-        this.nextPage = prev ? end + 1 : 0; // 2 페이징 이상히면, 앞의 페이징으로 증가 이하면 0 페이지로 설정
-
+        this.totalPage = this.pageNumList.size(); // 전체 페이징
     }
 
 }
